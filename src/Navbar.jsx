7aed4c29183bf23fa99d89ed0980eb81428f5bdd
mdnadimweb <mdnadim442244@gmail.com>
2025-05-20 20:37:44 +0600

    initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,22 @@
+function Navbar() {
+  return (
+    <>
+      <nav className="container">
+        <div className="logo">
+          <img src="./images/brand_logo.png" alt="logo image" />
+        </div>
+        <div className="navigation">
+          <ul>
+            <li>MENU</li>
+            <li>LOCATION</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+            <li>servise</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
