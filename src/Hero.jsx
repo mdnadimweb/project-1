c1c05f216f5a55fa1c67a05086de30547d11ff14
mdnadimweb <mdnadim442244@gmail.com>
2025-06-28 20:44:35 +0600

    project finish

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,21 +3,28 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-    <div className="container">
-        <div className="herocontent">
+    <div className="hero container">
+        <div className="hero_content">
     <h1>YOUR FEET DESERVE THE BEST</h1>
     <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
     
-    <div className="herobutton">
+    <div className="hero_button">
         <button>Shop Now</button>
-        <button>Category</button>
+        <button className='category'>Category</button>
     </div>
+      <div className="shopping_card">
+        <p>Also Available On</p>
+        <div className="shopping_logo">
+        <img src="/public/images/amazon.png" alt="" />
+        <img src="/public/images/flipkart.png" alt="" />
         </div>
-        <div className="heroimage"></div>
+      </div>
+        </div>
+        <div className="hero_image"> <img src="/public/images/shoe_image.png" alt="" /></div>
     </div>
     
     </>
   )
 }
 
-export default Hero
+export default Hero ; 
